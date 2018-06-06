47e0d9f97cbef45ee3e83f97974a619ab271bfb5
ali tamimi <al.timimi@gmail.com>
2018-06-06 10:29:19 +0000

    validations

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -30,7 +30,7 @@ class VenuesController < ApplicationController
 
     respond_to do |format|
       if @venue.save
-        format.html { redirect_to @venue, notice: 'Venue was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Venue was successfully created.' }
       else
         format.html { render :new }
       end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,6 @@ class Venue < ActiveRecord::Base
     belongs_to :user
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    
+    validates :description, :vSize, :num, :location, presence: true
 end
